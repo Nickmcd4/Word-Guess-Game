f70398c2793255914f64ce36ad320f97789293cb
NickMcD4 <nickmcd4@gmail.com>
2018-06-09 20:32:40 -0600

    started js coding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,71 @@
+var fishTypes = ["cod", "guppy","shark", "trout", "eel", "mackerel", "salmon", "goldfish" ];
+var wins = 0;
+var loss = 0;
+var wrongLetter = [];
+var guessesLeft = 9;
+var underScores = [];
+var userGuesses = [];
+var randWord;
+
+function startGame() {
+    randWord = fishTypes[Math.floor(Math.random() * fishTypes.length)];
+    console.log(randWord);
+
+    for (var i = 0; i < randWord.length; i++){
+        console.log(i);
+        underScores.push(" _ ");
+    }
+    console.log(underScores);
+
+    document.getElementById ("guesses").innerHTML = underScores.join("");
+
+    //new game
+    wrongLetter = [];
+    guessesLeft = 9;
+
+    document.getElementById("RemainingGuesses").innerHTML = guessesLeft;
+}
+
+document.onkeyup = function (event) {
+    userGuesses = event.key;
+    console.log(userGuesses);
+}
+
+startGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var sample = document.getElementById("foobar");
 sample.play();
+
